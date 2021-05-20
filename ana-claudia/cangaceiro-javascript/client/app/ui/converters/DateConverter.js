a0384ec942fdad3cf5b11d18141cc340ed4af95c
claudia2412 <claudia.2412@hotmail.com>
2021-05-19 23:03:14 -0300

    Completed fourth chapter

---
+++ ana-claudia/cangaceiro-javascript/client/app/ui/converters/DateConverter.js
@@ -0,0 +1,19 @@
+class DateConverter {
+
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static paraTexto(data) {
+    return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
+    }
+
+    static paraData(texto) {
+        //expressão regex
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        
+        return new Date(...texto.split('-')
+            .map((item, indice) => item - indice % 2));
+    }
+}
