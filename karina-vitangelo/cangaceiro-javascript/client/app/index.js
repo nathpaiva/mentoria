ec50f6488bc2e0b2e939cedb4c78cbff43b69d1a
Karina Vitangelo <karina.de.moraes@avanade.com>
2021-09-12 20:00:52 -0300

    realizado o yarn install e inseridos comentários adicionais no arquivo index.js

--- karina-vitangelo/cangaceiro-javascript/client/app/index.js
+++ karina-vitangelo/cangaceiro-javascript/client/app/index.js
@@ -15,20 +15,20 @@ document.querySelector('.form').addEventListener('submit', function (event) {
     var tr = document.createElement('tr');
 
     campos.forEach(function (campo) {
-        var td = document.createElement('td');
-        td.textContent = campo.value;
-        tr.appendChild(td);
+        var td = document.createElement('td'); //Cria uma td sem informações
+        td.textContent = campo.value; // atribui o valor do campo a tc
+        tr.appendChild(td); // adiciona a td na tr
     });
 
-    var tdVolume = document.createElement('td');
-    tdVolume.textContent = campos[1].value * campos[2].value;
-    tr.appendChild(tdVolume);
+    var tdVolume = document.createElement('td'); // nova td que armazena o volume da negociação
+    tdVolume.textContent = campos[1].value * campos[2].value; // as posições 1 e 2 do nosso array armazenam os campos de quantidade e valor
+    tr.appendChild(tdVolume); //adicionando a td que faltava na tr
 
     tbody.appendChild(tr);
 
-    // limpar campos
+    // limpar campos data, quantidade e valor
     campos[0].value = '';
     campos[1].value = 1;
     campos[2].value = 0;
-    campos[0].focus();
+    campos[0].focus(); // foca no campo da data
 });
