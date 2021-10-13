b03b49ef1ad46fd8b77e45e1f09d753a015b28da
fesanto <fgesanto@gmail.com>
2021-10-13 16:31:20 -0300

    changed all var for const and changed the function expression for arrow function

--- fernanda-santo/cangaceiro-javascript/client/app/index.js
+++ fernanda-santo/cangaceiro-javascript/client/app/index.js
@@ -1,6 +1,6 @@
 
 //array de campos com referência para cada um dos elementos de entrada do formulários
-var campos = [
+const campos = [
     document.querySelector('#date'),
     document.querySelector('#value'),
     document.querySelector('#quantity')
@@ -9,19 +9,19 @@ var campos = [
 console.log(campos);
 
 //buscando o elemento <tbody> pois é nele que inseriremos a <tr> que criaremos com os dados das negociações
-var tbody = document.querySelector('table tbody');
+const tbody = document.querySelector('table tbody');
 
-document.querySelector('.form').addEventListener('submit', function (event) {
-    
+document.querySelector('.form').addEventListener('submit', (event) => {
+
     //cancela o comportamento padrão
     event.preventDefault();
     //cria uma tr
-    var tr = document.createElement('tr');
+    const tr = document.createElement('tr');
 
     // para varrer o array campos e pegar cada um dos seus elementos
     campos.forEach(function (campo) {
         //cria uma td sem informações
-        var td = document.createElement('td');
+        const td = document.createElement('td');
         //atribui o valor do campo à td
         td.textContent = campo.value;
         //adiciona a td na tr
@@ -29,11 +29,11 @@ document.querySelector('.form').addEventListener('submit', function (event) {
     });
 
     //for (var campo of campos) {
-        //*** tentar desenvolver depois esse for mais simples que o forEach ***/
+    //*** tentar desenvolver depois esse for mais simples que o forEach ***/
     //}
 
     //cria a nova td que receberá o volume
-    var tdVolume = document.createElement('td');
+    const tdVolume = document.createElement('td');
     //atribui o produto da multiplicação entre valores dos index [1] e [2]
     tdVolume.textContent = campos[1].value * campos[2].value;
     //adiciona a td na tr
