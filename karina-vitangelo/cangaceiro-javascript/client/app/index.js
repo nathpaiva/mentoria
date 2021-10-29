d02a8928a272808157985d4484ec66a636cec3ae
Karina Vitangelo <karina.de.moraes@avanade.com>
2021-10-29 12:34:33 -0300

    Substituição das vars por let ou const

--- karina-vitangelo/cangaceiro-javascript/client/app/index.js
+++ karina-vitangelo/cangaceiro-javascript/client/app/index.js
@@ -1,26 +1,25 @@
 
-var campos = [
+const campos = [
     document.querySelector('#data'),
     document.querySelector('#valor'),
     document.querySelector('#quantidade')
 ];
 
-console.log(campos);
 
-var tbody = document.querySelector('table tbody');
+const tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', function (event) {
 
     event.preventDefault();
-    var tr = document.createElement('tr');
+    let tr = document.createElement('tr');
 
     campos.forEach(function (campo) {
-        var td = document.createElement('td'); //Cria uma td sem informações
+        let td = document.createElement('td'); //Cria uma td sem informações
         td.textContent = campo.value; // atribui o valor do campo a tc
         tr.appendChild(td); // adiciona a td na tr
     });
 
-    var tdVolume = document.createElement('td'); // nova td que armazena o volume da negociação
+    let tdVolume = document.createElement('td'); // nova td que armazena o volume da negociação
     tdVolume.textContent = campos[1].value * campos[2].value; // as posições 1 e 2 do nosso array armazenam os campos de quantidade e valor
     tr.appendChild(tdVolume); //adicionando a td que faltava na tr
 
@@ -32,3 +31,5 @@ document.querySelector('.form').addEventListener('submit', function (event) {
     campos[2].value = 0;
     campos[0].focus(); // foca no campo da data
 });
+
+
